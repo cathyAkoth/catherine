d2430ff0bd6df223c4aa71d49ccfe0e95d728bb0
CathyAkoth <catherinececilia22@gmail.com>
2021-11-16 10:28:12 -0800

    Added HomePage

--- src/App.js
+++ src/App.js
@@ -6,7 +6,12 @@ function App() {
     <div className="App">
       <Sidebar />
       <MainContentStyled>
-
+        <div className="lines">
+          <div className="line-1"></div>
+          <div className="line-2"></div>
+          <div className="line-3"></div>
+          <div className="line-4"></div>
+        </div>
       </MainContentStyled>
     </div>
   );
@@ -16,7 +21,18 @@ const MainContentStyled = styled.main`
   position:relative;
   margin-left:16.3rem;
   min-height:100vh;
-  background-color:red;
+
+  .lines{
+    position: absolute;
+    min-height: 100vh;
+    width:100%;
+    .line-1, .line-2, .line-3, .line-4{
+      width: .4rem;
+      min-height:100vh;
+      background-color:red;
+    }
+  }
+  
 `;
 
 
---
+++ src/Pages/HomePage.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+function HomePage(){
+return(
+    <HomePageStyled>
+
+    </HomePageStyled>
+)
+}
+
+const HomePageStyled = styled.header`
+    width:100%;
+    height:100vh;
+`;
+
+export default HomePage;
