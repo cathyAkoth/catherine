4a2a94da7f4d1bcab9d5f7fee9c6f5417f589d3e
CathyAkoth <catherinececilia22@gmail.com>
2021-11-17 05:55:26 -0800

    Added more style to title component

--- src/Components/Title.js
+++ src/Components/Title.js
@@ -1,11 +1,53 @@
 import React from 'react'
-
-function Title(){
+import styled from 'styled-components';
+function Title({title, span}){
     return(
-        <div>
-
-        </div>
+        <TitleStyled>
+            <h2>{title} <span>{span}</span></h2>
+        </TitleStyled>
     )
 }
+
+const TitleStyled = styled.div`
+    position: relative;
+    h2{
+        color: purple;
+        font-size:3rem;
+        font-weight:600;
+        text-transform: uppercase;
+        position:relative;
+        padding-bottom: .7rem;
+        &::before{
+            content:"";
+            position:absolute;
+            bottom: 0;
+            width:7.4rem;
+            height:.33rem;
+            background-color: rgb(120,0,120);
+            border-radius:15px;
+        }
+        &::after{
+            content:"";
+            position:absolute;
+            bottom: 0;
+            width:3.5rem;
+            height:.33rem;
+            background-color: var(--primary-color);
+            border-radius:15px;
+            left:0;
+        }
+        span{
+            font-weight:900;
+            color: rgba(25,29,43,44); 
+            font-size:3.4rem;
+            opacity:2.5;
+            position:absolute;
+            left: 0;
+            top: 30%;
+            z-index: -1;
+        }
+    }
+
+`;
 
 export default Title;
--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import styled from "styled-components";
 import {MainLayout} from '../styles/Layouts';
+import Title from "../Components/Title";
+
+
 function AboutPage(){
     return(
         <MainLayout>
             <AboutStyled>
-                <h1> hello </h1>
+                <Title title={'About Me'} span={'About Me'} />
             </AboutStyled>
         </MainLayout>
     )
