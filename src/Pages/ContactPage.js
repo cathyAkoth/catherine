6034afa4a1de35fb86d65098f1707ed3ae8f6546
CathyAkoth <catherinececilia22@gmail.com>
2021-11-18 12:51:08 -0800

    Added ContactItem component

--- src/Pages/ContactPage.js
+++ src/Pages/ContactPage.js
@@ -6,7 +6,7 @@ import PrimaryButton from '../Components/PrimaryButton';
 import PhoneIcon from '@material-ui/icons/Phone';
 import EmailIcon from '@material-ui/icons/Email';
 import LocationOnIcon from '@material-ui/icons/LocationOn';
-import ContactItem from '../Components/ContactItem';
+//import ContactItem from '../Components/ContactItem';
 
 function ContactPage() {
     const phone = <PhoneIcon />
@@ -43,12 +43,12 @@ function ContactPage() {
                         </div>
                     </form>
                 </div>
-                <div className="right-content">
+                {/* <div className="right-content">
                     <ContactItem title={'Phone'} icon={phone} cont1={'+66-789675637'} cont2={'07663520283'} />
                     <ContactItem title={'Email'} icon={email} cont1={'loremipsum@gmail.com'} cont2={'info.lorem.ipsum@gmail.com'} />
                     <ContactItem title={'Address'} icon={location} cont1={'27 Aldrich Road, London, England'} cont2={'United Kingdom'} />
                     
-                </div>
+                </div> */}
             </InnerLayout>
             </ContactPageStyled>
         </MainLayout>
@@ -105,7 +105,7 @@ const ContactPageStyled = styled.section`
                     height: 50px;
                     padding:0 15px;
                     width: 100%;
-                    color: inherit;
+                    color: purple;
                 }
                 textarea{
                     background-color: transparent;
