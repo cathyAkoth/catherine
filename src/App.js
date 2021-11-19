854f459199a6884b85cb71b61a8293144d38ef6b
CathyAkoth <catherinececilia22@gmail.com>
2021-11-19 05:18:28 -0800

    Added light-dark-mode

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import Sidebar from "./Components/Sidebar";
 import styled from "styled-components";
 import HomePage from "./Pages/HomePage";
-import {Route, Switch} from "react-router-dom";
+import {Route, Switch as Switching} from "react-router-dom";
 import BlogsPage from "./Pages/BlogsPage";
 import ContactPage from "./Pages/ContactPage";
 import AboutPage from "./Pages/AboutPage";
 import ResumePage from "./Pages/ResumePage";
 import PortfolioPage from "./Pages/PortfoliosPage";
+import Brightness4Icon from '@material-ui/icons/Brightness4';
+
+import  Switch  from "@material-ui/core/Switch";
+
 function App() {
   return (
     <div className="App">
@@ -19,7 +23,22 @@ function App() {
           <div className="line-4"></div>
         </div> */}
 
-        <Switch>
+        <div className = "light-dark-mode">
+          <div className="left-content">
+            <Brightness4Icon />
+          </div>
+          <div className="right-content">
+            <Switch
+              value=""
+              // checked={}
+              // onChange={}
+              inputProps={{'aria-label': ''}}
+              size="medium"
+            />
+          </div>
+        </div>
+
+        <Switching>
           <Route path="/" exact>
             <HomePage />
           </Route>
@@ -38,7 +57,7 @@ function App() {
           <Route path="/blogs" exact>
             <BlogsPage />
           </Route>
-        </Switch>
+        </Switching>
 
       </MainContentStyled>
     </div>
@@ -49,6 +68,24 @@ const MainContentStyled = styled.main`
   position:relative;
   margin-left:16.3rem;
   min-height:100vh;
+
+  .light-dark-mode{
+    position: absolute;
+    right:0;
+    top: 10%;
+    background-color: var(--primary-color);
+    width: 6.5rem;
+    height: 2.5rem;
+    z-index: 15;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    svg{
+      display: flex;
+      align-items: center;
+      font-size: 1.7rem;
+    }
+  }
 
   .lines{
     position: absolute;
