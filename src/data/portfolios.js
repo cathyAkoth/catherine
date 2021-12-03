0d8a97587eef2c9cd5a5a90af8990bcdd45c54bc
CathyAkoth <catherinececilia22@gmail.com>
2021-12-02 22:52:57 -0800

    Edited portfolios.js

--- src/data/portfolios.js
+++ src/data/portfolios.js
@@ -1,72 +1,72 @@
-import css1 from '../img/websitehow.jpg';
-import css2 from '../img/websitehow.jpg';
-import react1 from '../img/websitehow.jpg';
-import img2 from '../img/websitehow.jpg';
-import img3 from '../img/websitehow.jpg';
+// import css1 from '../img/websitehow.jpg';
+// import css2 from '../img/websitehow.jpg';
+// import react1 from '../img/websitehow.jpg';
+// import img2 from '../img/websitehow.jpg';
+// import img3 from '../img/websitehow.jpg';
 
-const portfolios = [
-    {
-        id: 1,
-        category: 'CSS',
-        image: css1,
-        link1: 'https://www.google.com',
-        link2: 'https://www.google.com',
-        title: 'Responsive Resume',
-        text: 'Created with only HTML and CSS.'
-    },
-    {
-        id: 2,
-        category: '.net C#',
-        image: img3,
-        link1: 'https://www.google.com',
-        link2: 'https://www.google.com',
-        title: 'How To Animate In Maya',
-        text: 'Number one Animation Application'
-    },
-    {
-        id: 3,
-        category: 'React.js',
-        image: img3,
-        link1: 'https://www.google.com',
-        link2: 'https://www.google.com',
-        title: 'How To Animate In Maya',
-        text: 'Number one Animation Application'
-    },
-    {
-        id: 4,
-        category: 'Animation',
-        image: img2,
-        link1: 'https://www.google.com',
-        link2: 'https://www.google.com',
-        title: 'How To Use Blender',
-        text: 'Free Animation Software'
-    },
-    {
-        id: 5,
-        category: 'node.js',
-        image: img2,
-        link1: 'https://www.google.com',
-        link2: 'https://www.google.com',
-        title: 'How To Use Blender',
-        text: 'Free Animation Software'
-    },
-    {
-        id: 6,
-        category: 'React.js',
-        image: react1,
-        link1: 'https://catherineakoth.herokuapp.com',
-        title: 'Responsive Portfolio Website',
-        text: 'Created using different technologies such as Material UI, Styled-Components and more...'
-    },
-    {
-        id: 7,
-        category: 'CSS',
-        image: css2,
-        link1: 'https://www.google.com',
-        link2: 'https://www.google.com',
-        title: 'Microsoft Website Clone',
-        text: 'Created using HTML and CSS'
-    }
-]
+// const portfolios = [
+//     {
+//         id: 1,
+//         category: 'CSS',
+//         image: css1,
+//         link1: 'https://www.google.com',
+//         link2: 'https://www.google.com',
+//         title: 'Responsive Resume',
+//         text: 'Created with only HTML and CSS.'
+//     },
+//     {
+//         id: 2,
+//         category: '.net C#',
+//         image: img3,
+//         link1: 'https://www.google.com',
+//         link2: 'https://www.google.com',
+//         title: 'How To Animate In Maya',
+//         text: 'Number one Animation Application'
+//     },
+//     {
+//         id: 3,
+//         category: 'React.js',
+//         image: img3,
+//         link1: 'https://www.google.com',
+//         link2: 'https://www.google.com',
+//         title: 'How To Animate In Maya',
+//         text: 'Number one Animation Application'
+//     },
+//     {
+//         id: 4,
+//         category: 'Animation',
+//         image: img2,
+//         link1: 'https://www.google.com',
+//         link2: 'https://www.google.com',
+//         title: 'How To Use Blender',
+//         text: 'Free Animation Software'
+//     },
+//     {
+//         id: 5,
+//         category: 'node.js',
+//         image: img2,
+//         link1: 'https://www.google.com',
+//         link2: 'https://www.google.com',
+//         title: 'How To Use Blender',
+//         text: 'Free Animation Software'
+//     },
+//     {
+//         id: 6,
+//         category: 'React.js',
+//         image: react1,
+//         link1: 'https://catherineakoth.herokuapp.com',
+//         title: 'Responsive Portfolio Website',
+//         text: 'Created using different technologies such as Material UI, Styled-Components and more...'
+//     },
+//     {
+//         id: 7,
+//         category: 'CSS',
+//         image: css2,
+//         link1: 'https://www.google.com',
+//         link2: 'https://www.google.com',
+//         title: 'Microsoft Website Clone',
+//         text: 'Created using HTML and CSS'
+//     }
+// ]
 
-export default portfolios;
+// export default portfolios;
