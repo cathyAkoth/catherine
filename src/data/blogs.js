63b94d402eb9325ae62651b8988d858bda666aaa
CathyAkoth <catherinececilia22@gmail.com>
2021-12-02 22:50:58 -0800

    Edited blogs.js

--- src/data/blogs.js
+++ src/data/blogs.js
@@ -1,47 +1,47 @@
-import blog1 from  '../img/websitehow.jpg';
-import blog3 from  '../img/websitehow.jpg';
-import blog2 from  '../img/websitehow.jpg';
-import blog4 from  '../img/websitehow.jpg';
+// import blog1 from  '../img/websitehow.jpg';
+// import blog3 from  '../img/websitehow.jpg';
+// import blog2 from  '../img/websitehow.jpg';
+// import blog4 from  '../img/websitehow.jpg';
 
 
-const blogs = [
-    {
-        id: 1,
-        title: 'How To Work from Home',
-        date: '01',
-        month: 'April',
-        image: blog1,
-        link: 'https://www.youtube.com/watch?v=w8I1m1n-7Lg/'
-    },
-    {
-        id: 2,
-        title: 'How To Use SEO Efficiently',
-        date: '01',
-        month: 'April',
-        image: blog2
-    },
-    {
-        id: 3,
-        title: 'How to choose a programming Language',
-        date: '01',
-        month: 'April',
-        image: blog3,
-        link: 'https://www.google.co.uk/'
-    },
-    {
-        id: 4,
-        title: 'How To Tse SEO Efficiently',
-        date: '01',
-        month: 'April',
-        image: blog4
-    },
-    {
-        id: 5,
-        title: 'How To Tse SEO Efficiently',
-        date: '01',
-        month: 'April',
-        image: blog4
-    }
-]
+// const blogs = [
+//     {
+//         id: 1,
+//         title: 'How To Work from Home',
+//         date: '01',
+//         month: 'April',
+//         image: blog1,
+//         link: 'https://www.youtube.com/watch?v=w8I1m1n-7Lg/'
+//     },
+//     {
+//         id: 2,
+//         title: 'How To Use SEO Efficiently',
+//         date: '01',
+//         month: 'April',
+//         image: blog2
+//     },
+//     {
+//         id: 3,
+//         title: 'How to choose a programming Language',
+//         date: '01',
+//         month: 'April',
+//         image: blog3,
+//         link: 'https://www.google.co.uk/'
+//     },
+//     {
+//         id: 4,
+//         title: 'How To Tse SEO Efficiently',
+//         date: '01',
+//         month: 'April',
+//         image: blog4
+//     },
+//     {
+//         id: 5,
+//         title: 'How To Tse SEO Efficiently',
+//         date: '01',
+//         month: 'April',
+//         image: blog4
+//     }
+// ]
 
-export default blogs;
+// export default blogs;
